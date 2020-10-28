73dea0dbba5a634d51dffa16b4b4051cc7d65a56
Jonathan Capes <j.capes@gmail.com>
2020-10-28 14:35:19 -0700

    algolia config

--- config.js
+++ config.js
@@ -28,13 +28,13 @@ const config = {
 		    </a>
 		  </li>`,
     links: [{ text: '', link: '' }],
-    search: {
-      enabled: false,
-      indexName: '',
-      algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
-      algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
-      algoliaAdminKey: process.env.ALGOLIA_ADMIN_KEY,
-    },
+    "search": {
+  		"enabled": true,
+  		"indexName": "tutorial_index",
+  		"algoliaAppId": process.env.GATSBY_ALGOLIA_APP_ID,
+  		"algoliaSearchKey": process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+  		"algoliaAdminKey": process.env.ALGOLIA_ADMIN_KEY
+  	},
   },
   sidebar: {
     forcedNavOrder: [

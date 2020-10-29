6115bf573d4bdd71e3357a9d7861605a87c3467b
Jonathan Capes <j.capes@gmail.com>
2020-10-28 23:02:03 -0700

    logo embed update

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const config = {
     trailingSlash: false,
   },
   header: {
-    logo: 'src/component/images/amazonlumberyard.svg',
+    logo: {logoImg},
     logoLink: 'https://tutorial-portfolio.netlify.app/',
     title:
       "<a href='https://tutorial-portfolio.netlify.app/'><img class='img-responsive' src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/learn-logo.svg' alt='Learn logo' /></a>",

9aeba2ff004aabadb078e4d2653a7ce60738a08a
Jonathan Capes <j.capes@gmail.com>
2020-10-28 22:22:30 -0700

    LY icon added to config

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const config = {
     trailingSlash: false,
   },
   header: {
-    logo: 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/brand.svg',
+    logo: 'src/components/images/amazonlumberyard.svg',
     logoLink: 'https://tutorial-portfolio.netlify.app/',
     title:
       "<a href='https://tutorial-portfolio.netlify.app/'><img class='img-responsive' src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/learn-logo.svg' alt='Learn logo' /></a>",
